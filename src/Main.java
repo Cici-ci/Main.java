e7ceebdf93f3863789c6aace0e274c1c533cda98
Cici <yslstryhhh1@gmail.com>
2022-06-29 16:40:09 +0800

    增加printMortgage()和printPaymentSchedule（）方法

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,32 @@ public class Main {
     final static  byte MONTHS_IN_YEAR = 12;
     final static  byte PERCENT = 100;
     public static void main(String[] args) {
-        int principal = (int) readNumber("Enter the principal:", 100, 10000);
-        float annualInterest = (float) readNumber("Enter the Annual Interest Rate:", 1, 10);
-        int years = (int) readNumber("Enter the period:", 1, 100);
+        int principal = (int) readNumber("Enter the principal: ", 100, 10000);
+        float annualInterest = (float) readNumber("Enter the Annual Interest Rate: ", 1, 10);
+        int years = (int) readNumber("Enter the period: ", 1, 100);
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
 
-        double mortgage=calculatorMortgage(principal,annualInterest,years);
+    public static void printPaymentSchedule(int principal, float annualInterest, int years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("-----------------");
+        for (short month = 1; month<= years *MONTHS_IN_YEAR; month++){
+            double balance = calculateBalance(principal, annualInterest, years,month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public static void printMortgage(int principal, float annualInterest, int years) {
+        double mortgage=calculatorMortgage(principal, annualInterest, years);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Mortgage "+NumberFormat.getCurrencyInstance().format(mortgage));
-
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("-----------------");
-        for (short month =1; month<=years*MONTHS_IN_YEAR;month++){
-            double balance = calculateBalance(principal,annualInterest,years,month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-            System.out.println("---");
-
-        }
     }
-        static double readNumber (String prompt,double min, double max){
+
+    static double readNumber (String prompt,double min, double max){
             Scanner scanner = new Scanner(System.in);
             double value;
             while (true) {
